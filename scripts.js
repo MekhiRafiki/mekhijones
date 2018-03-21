9bed6e8a854cd00f7ac6927f4b6ab1afbd7957f6
Mekhi Jones <mekhi.rafiki@gmail.com>
2018-03-20 17:57:59 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+<script>
+function myFunction(){
+  var x = document.getElementById("ex");
+  if(x.style.display === "none"){
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+</script>
